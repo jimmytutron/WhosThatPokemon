957484701b0c483a2e31508fe4ddfcf5021ba130
jimmytu <jimmytutron@gmail.com>
2018-05-02 19:19:38 -0700

    Create index and files, begin pseudo coding

---
+++ assets/js/porygon.js
@@ -0,0 +1,15 @@
+// I heard you wanna be a Pokemon Master???
+// When the page loads, intialize the game with a start button. When the user presses the start button the game will begin.
+
+// one question or slide will be displayed and there will be a counter that's decrementing the time remaining to answer.
+
+// the user has a certain amount of time to select an answer. They can only pick once and cannot change their answer.
+
+// When the player clicks an answer it will tell the player whether or not they were correct and then display the correct answer. 
+// The correct answer will be displayed for a short time before automatically moving to the next question
+
+// if the player does not make a selection and the timer ticks down to 0, 
+// then the correct answer will display and will automatically move to the next question.
+
+// When all the questions have been answered, display the user's correct answers, wrong answers, and skipped answer. 
+// A reset button will allow the user to play again without resetting the game.
