f4870b9361d86e218a67c5e18c082ed229ea53cd
jimmytu <jimmytutron@gmail.com>
2018-05-03 21:55:31 -0700

    STLYING, styling, so much styling

--- assets/js/porygon.js
+++ assets/js/porygon.js
@@ -29,61 +29,85 @@ var whoDat = [{
 		pokemon: "assets/imgs/pokemon1.png",
 		pokemonAnswer: "assets/imgs/pokemon1answer.png",
 		answers: ["Raichu","Pikachu","Pichu","Pachirishu"],
+		gifRight: "https://m.popkey.co/7d5b9a/47oQR.gif",
+		gifWrong: "https://www.reactiongifs.us/wp-content/uploads/2014/08/pikachu_crying_pokemon.gif",
 		correct: 1
 	},{
 		pokemon: "assets/imgs/pokemon2.png",
 		pokemonAnswer: "assets/imgs/pokemon2answer.png",
+		gifRight: "https://pa1.narvii.com/6176/c74939814e4a6c079d2cc964921f1c20a4165338_hq.gif",
+		gifWrong: "https://i.pinimg.com/originals/26/79/9c/26799c63d600b3a9e85f659f1c7630f6.gif",
 		answers: ["Chansey","Jigglypuff","Audino","Clefairy"],
 		correct: 3
 	},{
 		pokemon: "assets/imgs/pokemon3.png",
 		pokemonAnswer: "assets/imgs/pokemon3answer.png",
+		gifRight: "https://i.gifer.com/8Xvg.gif",
+		gifWrong: "https://pa1.narvii.com/6485/61b18568f3975b047091198800fb4f8ae34e12b5_hq.gif",
 		answers: ["Vaporeon","Flareon","Jolteon","Espeon"],
 		correct: 0
 	},{
 		pokemon: "assets/imgs/pokemon4.png",
 		pokemonAnswer: "assets/imgs/pokemon4answer.png",
+		gifRight: "https://pa1.narvii.com/6530/01392bf0844bf359323b9e8eb4c1cebd8e57a83d_hq.gif",
+		gifWrong: "https://i1.wp.com/68.media.tumblr.com/f9ad7eea96e999b50c82f8884a86546a/tumblr_owarlwD5VT1qioqevo2_1280.gif",
 		answers: ["Dodrio","Dugtrio","Oricorio","Lucario"],
 		correct: 1
 	},{
 		pokemon: "assets/imgs/pokemon5.png",
 		pokemonAnswer: "assets/imgs/pokemon5answer.png",
+		gifRight: "https://pa1.narvii.com/6232/cc07b298f596324e179f6ddef89412b43adef232_hq.gif",
+		gifWrong: "https://i.gifer.com/7ttG.gif",
 		answers: ["Exeggcute","Cleffa","Togepi","Marill"],
 		correct: 2
 	},{
 		pokemon: "assets/imgs/pokemon6.png",
+		gifRight: "https://thumbs.mic.com/Y2VjNzZjYjZiMyMvVHE0dkdNWEM0Z3prTVBHdkJNZTdyV09XZ2l3PS9maXQtaW4vNzYweDAvZmlsdGVyczpub191cHNjYWxlKCk6Zm9ybWF0KGpwZWcpOnF1YWxpdHkoODApL2h0dHBzOi8vczMuYW1hem9uYXdzLmNvbS9wb2xpY3ltaWMtaW1hZ2VzL3ZyaWF1Y3JweXNwdTgzeHE4eGppazFqZm1pZWdhZ3N1d2M5amw2cWptdnFsazlvcmdsMWxxNXRvZ2xnbm9uYWMuZ2lm.gif",
+		gifWrong: "https://www.pokecommunity.com/signaturepics/sigpic660252_2.gif",
 		pokemonAnswer: "assets/imgs/pokemon6answer.png",
 		answers: ["Dragonite","Salamence","Tyranitar","Garchomp"],
 		correct: 2
 	},{
 		pokemon: "assets/imgs/pokemon7.png",
+		gifRight: "http://i.imgur.com/LxN7ai3.gif",
+		gifWrong: "https://media.giphy.com/media/TUaKA70WsyC64/giphy.gif",
 		pokemonAnswer: "assets/imgs/pokemon7answer.png",
 		answers: ["Blaziken","Latias","Latios","Noivern"],
 		correct: 2
 	},{
 		pokemon: "assets/imgs/pokemon8.png",
+		gifRight: "https://78.media.tumblr.com/c93a7ca4f9cbbceaf8dd45aa16c6c6ae/tumblr_mpsoghDOso1r5fhkdo1_500.gif",
+		gifWrong: "https://25.media.tumblr.com/tumblr_m65131tJ5u1qlms3vo1_500.gif",
 		pokemonAnswer: "assets/imgs/pokemon8answer.png",
 		answers: ["Butterfree","Venemoth","Dustox","Beautifly"],
 		correct: 3
 	},{
 		pokemon: "assets/imgs/pokemon9.png",
+		gifRight: "https://pa1.narvii.com/5905/d5d0abc5ba30604eefe7165b73fa1553ee5e5e65_hq.gif",
+		gifWrong: "https://i.chzbgr.com/full/8535999744/h2A85AF85/",
 		pokemonAnswer: "assets/imgs/pokemon9answer.png",
 		answers: ["Klink","Klang","Klefki","Klinklang"],
 		correct: 2
 	},{
 		pokemon: "assets/imgs/pokemon10.png",
+		gifRight: "https://sixprizes.com/wp-content/uploads/2016/03/greninja-nod-3-2.gif",
+		gifWrong: "http://i0.kym-cdn.com/photos/images/newsfeed/001/152/546/aba.gif",
 		pokemonAnswer: "assets/imgs/pokemon10answer.png",
 		answers: ["Greninja","Pangoro","Zoroark","Bisharp"],
 		correct: 0
 	},{
 		pokemon: "assets/imgs/pokemon11.png",
+		gifRight: "http://gifimage.net/wp-content/uploads/2017/10/mimikyu-gif-3.gif",
+		gifWrong: "https://i0.wp.com/68.media.tumblr.com/25f1ab69f4774d4cf68f6ad12aa1b774/tumblr_ouhdif5fmK1qioqevo1_1280.gif",
 		pokemonAnswer: "assets/imgs/pokemon11answer.png",
 		answers: ["Raichu","Pikachu","Mimikyu","Pachirishu"],
 		correct: 2
 	},{
 		pokemon: "assets/imgs/pokemon12.png",
+		gifRight: "http://31.media.tumblr.com/fbef48d53228a27267ed35d1a5660643/tumblr_mxd1v7DObd1qau6z9o1_r1_500.gif",
+		gifWrong: "https://78.media.tumblr.com/d3480bf2785e4cf37614d9d124a59c4e/tumblr_p1oi2a16T21wof669o1_500.gif",
 		pokemonAnswer: "assets/imgs/pokemon12answer.png",
-		answers: ["Voltorb","Electrode","Solosis","Jigglypuff as seen from above"],
+		answers: ["Voltorb","Electrode","Solosis","Jigglypuff"],
 		correct: 2
 	}
 	];
@@ -118,10 +142,10 @@ function newPokemon(){
 
 	$("#correctPokemon").empty();
 
- 		$("#pokemon").html("<img class='pkmnImg img-fluid' src=" + whoDat[currentPokemon].pokemon + ">");
+ 		$("#pokemon").html("<img class='pkmnImg img-fluid animated bounceInLeft' src=" + whoDat[currentPokemon].pokemon + ">");
  		for (var i=0; i < 4; i++){
  			var choices = $('<div>');
- 			choices.html("<h2 class='btn btn-2 btn-2h'>" + whoDat[currentPokemon].answers[i] + "</h2>");
+ 			choices.html("<h2 class='btn btn-2 btn-2h animated bounceInRight'>" + whoDat[currentPokemon].answers[i] + "</h2>");
  			choices.attr("data-index", i);
  			choices.addClass("thisPokemon");
  			$("#answers").append(choices);
@@ -156,26 +180,30 @@ function displayPokemon(){
 
  		var correctIndex = whoDat[currentPokemon].correct
  		var correctAnswerText = whoDat[currentPokemon].answers[correctIndex];
+ 		var rightGif = "<img class='img-fluid animated bounceInRight' src='" +whoDat[currentPokemon].gifRight + "'>";
+ 		var wrongGif = "<img class='img-fluid animated bounceInRight' src='" +whoDat[currentPokemon].gifWrong + "'>";
 
  		if(userChoice === correctIndex && answered === true){
  			correctAnswers++;
- 			$("#correctPokemon").html("It's " + correctAnswerText);
- 			$("#pokemon").html("<img class='pkmnImg img-fluid' src=" + whoDat[currentPokemon].pokemonAnswer + ">");
+ 			$("#correctPokemon").html("<h2 class='animated bounceInRight'> It's " + correctAnswerText + "!</h2>");
+ 			$("#correctPokemon").append(rightGif);
+ 			$("#pokemon").html("<img class='pkmnImg img-fluid animated bounceInLeft' src=" + whoDat[currentPokemon].pokemonAnswer + ">");
  		} else if (userChoice != correctIndex && answered === true){
  			incorrectAnswers++;
- 			$("#correctPokemon").html("It's " + correctAnswerText);
- 			$("#pokemon").html("<img class='pkmnImg img-fluid' src=" + whoDat[currentPokemon].pokemonAnswer + ">");
+ 			$("#correctPokemon").html("<h2 class='animated bounceInRight'> Wrong! It's " + correctAnswerText+ "!</h2>");
+ 			$("#correctPokemon").append(wrongGif);
+ 			$("#pokemon").html("<img class='pkmnImg img-fluid animated bounceInLeft' src=" + whoDat[currentPokemon].pokemonAnswer + ">");
  		} else {
  			unanswered++;
- 			$("#correctPokemon").html("It's " + correctAnswerText);
- 			$("#pokemon").html("<img class='pkmnImg img-fluid' src=" + whoDat[currentPokemon].pokemonAnswer + ">");
+ 			$("#correctPokemon").html("<h2 class='animated bounceInRight'> You ran out of time! It's " + correctAnswerText+ "!</h2>");
+ 			$("#pokemon").html("<img class='pkmnImg img-fluid animated bounceInLeft' src=" + whoDat[currentPokemon].pokemonAnswer + ">");
  			answered = true;
  		}
  		if (currentPokemon === whoDat.length -1){
- 			setTimeout(results, 1000)
+ 			setTimeout(results, 7000)
  		}else{
  			currentPokemon++;
- 			setTimeout(newPokemon, 1000);
+ 			setTimeout(newPokemon, 7000);
  		}
 };
 function results(){
@@ -188,8 +216,13 @@ function results(){
 	$("#unanswered").html("Unanswered: " + unanswered);
 	restart.addClass("restart");
 	restart.show();
-	restart.html("Play Again?");
+	restart.html("<h2 class='btn btn-2 btn-2h'>Play Again?</h2>");
 };
+
+
+
+
+
 
 
 
